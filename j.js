254d23b0b627032e52746f49ed232c1d46e82074
480147530332 <145145478+480147530332@users.noreply.github.com>
2024-02-06 20:43:19 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,36 @@
+// function color (z){
+//     var   a =getComputedStyle(z);
+//     var b = a.backgroundColor;
+//     document.getElementsByTagName("BODY")[0].style.backgroundColor=b
+// }
+
+
+// function color(z){
+//     var a =getComputedStyle(z);
+//     var b = a.backgroundColor;
+//     document.getElementsByTagName("body")[0].style.backgroundColor=b
+// }
+
+// function color(value){
+// document.body.style.backgroundColor=value;
+
+// console.log(value);
+// }
+var x=0
+function imgchanger() 
+{
+    if(x==0)
+    {document.getElementById("pic").src="img/m1.jpg";
+    x++
+
+}
+else{
+    document.getElementById("pic").src="img/s2.jpg";
+    x=0
+}
+}
+// function imgchange()
+// {
+    // document.getElementById("pic2").src="img/img3.jpeg";
+// }
+
